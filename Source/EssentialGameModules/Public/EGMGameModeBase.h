15e735f16aeba8d568f35bc98534585fedd5c2ac
Kushagra <kushagra.mckv@gmail.com>
2023-01-25 19:49:52 +0530

    KP
    - Added CPP Support for the Project
    - Added a Base Game Mode

---
+++ Source/EssentialGameModules/Public/EGMGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "EGMGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESSENTIALGAMEMODULES_API AEGMGameModeBase : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
